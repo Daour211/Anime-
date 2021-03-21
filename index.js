5feb4f6f9bc2a60899df165a91b2405e3e7a9514
Abdelrahman Mohammad Al Daour <Daour.995@outlook.com>
2021-03-21 14:35:32 +0200

    modified HTML file and added FOR & WHILE loops on JS file

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+alert('Welcome to the Top Anime Website'); 
 
 var question1 = 'Who is your favourite character in one piece, luffy or zoro?';
 
@@ -5,12 +6,17 @@ alert(question1);
 
 var answer1 = prompt("your answer");
 
+// Making a loop to get the answer for the question either zoro or luffy 
+
+while ( answer1 !== 'luffy' && answer1 !== 'zoro'){
+  answer1 = prompt('your answer must be either luffy or zoro' ) 
+}
 if (answer1 === 'luffy') {
 
-    document.write('<img src="https://i.pinimg.com/474x/47/e2/3e/47e23ef9f58145646746b7e911b79c00.jpg"/>')
+    document.write('<img src="https://i.pinimg.com/474x/47/e2/3e/47e23ef9f58145646746b7e911b79c00.jpg"/> <br>')
 
 } else if (answer1 === 'zoro') {
-    document.write('<img src="https://mangathrill.com/wp-content/uploads/2020/12/daaaqweasdasdaqweasdasd.jpg"/>')
+    document.write('<img src="https://mangathrill.com/wp-content/uploads/2020/12/daaaqweasdasdaqweasdasd.jpg"/ , width= 350> <br>')
 }
 
 var question2 = 'Who is your favourite character in Naruto, naruto or sasuke?';
@@ -18,6 +24,10 @@ var question2 = 'Who is your favourite character in Naruto, naruto or sasuke?';
 alert(question2);
 
 var answer2 = prompt("your answer");
+
+while ( answer2 !== 'naruto' && answer2!== 'sasuke') {
+  answer2 = prompt("your answer must be either naruto or sasuke")
+}
 if (answer2 === 'naruto'){
 
     document.write('<img src="https://64.media.tumblr.com/cb22e5c2e2cdd57bc1c66012aa0db077/tumblr_pjznxwjt3w1wnec3vo1_250.gifv"/>')
@@ -33,13 +43,24 @@ alert(question3);
 
 var answer3 = prompt("your answer");
 
-document.write('<h3> you can say: May he/she rest in peace </h3?');
+document.write('<h3> you can say: May he/she rest in peace </h3>');
+ 
+// Asking for Death Note posters
+
+var dnPoster = '';
+var numberOfdn = prompt("How many posters you want to buy for Death Note?");
+
+for (var i = 1;i<=numberOfdn;i++){
+  dnPoster = dnPoster + '<img src= "https://cdn.europosters.eu/image/750/posters/death-note-duo-i28405.jpg"/> ' + i+ '<br>' ; 
+}
+
+document.write(dnPoster);
 
 var txt;
-var r = confirm("Press a button!");
+var r = confirm("Do you wish to know what will happen in the next episode of One Piece!");
 if (r == true) {
-  txt = "You pressed OK!";
+  txt = "You need to go and read the manga!";
 } else {
-  txt = "You pressed Cancel!";
+  txt = "Then you shall wait!";
 }
 document.write('<br> <p> ' + txt + ' </p> </br> ');
